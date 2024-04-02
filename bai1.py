9295a807ea8e3d11c622247b750b439a2dee65b0
NTT203 <anhtienpro203@gmail.com>
2024-04-02 08:10:23 +0700

    remove file

--- bai1.py
+++
@@ -1,9 +0,0 @@
-import itertools
-
-def print_permutations(lst):
-    permutations = list(itertools.permutations(lst))
-    for permutation in permutations:
-        print(permutation)
-
-
-print_permutations([1, 2, 3])
